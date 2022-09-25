30b32dc05ff18ba70e35bc7323fd9ffbec0417d8
André R.B. (mac) <bronca.andre@gmail.com>
2022-09-24 21:34:23 -0300

    properties do javamail para envio do e-mail

--- enviando-mail/src/test/java/com/arb/enviando_mail/AppTest.java
+++ enviando-mail/src/test/java/com/arb/enviando_mail/AppTest.java
@@ -15,10 +15,12 @@ public class AppTest {
 		 * https://www.tutorialspoint.com/javamail_api/javamail_api_gmail_smtp_server.htm
 		 */
 		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");	//autorização
+		properties.put("mail.smtp.starttls.enable", "true");	//autenticação
+		properties.put("mail.smtp.host", "smtp.gmail.com");	//servidor do gmail
+		properties.put("mail.smtp.port", "465");	//465 ou 587
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		
 	}
 }
