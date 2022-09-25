44e1be69657c450068fa4893286371c8694b27c4
André R.B. (mac) <bronca.andre@gmail.com>
2022-09-24 21:45:45 -0300

    configurando

--- enviando-mail/src/test/java/com/arb/enviando_mail/AppTest.java
+++ enviando-mail/src/test/java/com/arb/enviando_mail/AppTest.java
@@ -2,6 +2,10 @@ package com.arb.enviando_mail;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import org.junit.Test;
 
 /**
@@ -14,13 +18,27 @@ public class AppTest {
 		/* verificar as configurações do smtp 
 		 * https://www.tutorialspoint.com/javamail_api/javamail_api_gmail_smtp_server.htm
 		 */
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");	//autorização
-		properties.put("mail.smtp.starttls.enable", "true");	//autenticação
-		properties.put("mail.smtp.host", "smtp.gmail.com");	//servidor do gmail
-		properties.put("mail.smtp.port", "465");	//465 ou 587
-		properties.put("mail.smtp.socketFactory.port", "465");
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true");	//autorização
+			properties.put("mail.smtp.starttls.enable", "true");	//autenticação
+			properties.put("mail.smtp.host", "smtp.gmail.com");	//servidor do gmail
+			properties.put("mail.smtp.port", "465");	//465 ou 587
+			properties.put("mail.smtp.socketFactory.port", "465");
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return PasswordAuthentication()
+				}
+			});
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
